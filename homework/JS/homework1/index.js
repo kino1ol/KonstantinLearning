df955659f290c15b146899cb0d4d3ab400b5b9f5
JoneKonstanta <superkostia2000@gmail.com>
2023-03-13 15:08:09 +0300

    js first HW

---
+++ homework/JS/homework1/index.js
@@ -0,0 +1,31 @@
+let txt = "привет";
+const num = 123;
+let sum = 15.8;
+let flag = true; 
+let str = "true";
+
+let a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14; 
+
+alert(txt)
+console.log("txt, num, sum, flag, str")
+console.log(txt, num, sum, flag, str)
+console.log(typeof txt,typeof num,typeof sum,typeof flag,typeof str)
+
+let st = ""
+
+text1 = '"А ну-ка, спой!.."'
+text2 = '- и я пою.'
+
+console.log(st)
+console.log("Бывало, спит у ног собака,")
+console.log("костер занявшийся гудит,")
+console.log("и женщина из полумрака")
+console.log("глазами зыбкими глядит.")
+console.log(st)
+console.log(st)
+console.log("Потом под пихтою приляжет")
+console.log("на куртку рыжую мою")
+console.log("и мне, задумчивая, скажет:")
+console.log(st)
+console.log(st)
+console.log(text1 + text2)
